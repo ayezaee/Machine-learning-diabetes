c246c059da95e9b00de4007471b90debd4bad5e6
Xavier <x.reaves@yahoo.com>
2024-03-20 00:33:43 -0500

    Update WebApp.py

--- WebApp.py
+++ WebApp.py
@@ -13,10 +13,10 @@ st.write("""
  To show who is more likely to have diabetes 
 """)
 
-image = Image.open('C:/Users/xreav/ML project/diabetes.jpg')
+image = Image.open('{DIR}')
 st.image(image, caption='Machine Learning', use_column_width=True)
 
-df = pd.read_csv('C:/Users/xreav/ML project/diabetes.csv')
+df = pd.read_csv('{DIR}')
 
 st.subheader('Data Information:')
 
